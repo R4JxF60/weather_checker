b2ff3c8dae52b3a746f44f5cfa46ecada6dc930f
Rajitha Kumara <rajithakumaraprog@gmail.com>
2022-04-30 21:30:03 +0530

    remove: unwanted comment

--- src/utils/geofinder.js
+++ src/utils/geofinder.js
@@ -1,5 +1,3 @@
-// const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodedLocation}&key=b08bd4c6d1d84ad090bee3f82821883a`
-
 const axios = require('axios').default
 const http = require('http')
 const encodeurl = require('encodeurl')
